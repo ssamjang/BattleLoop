9ca675307e11f6e5df6c23fa71e5144d4d783231
Jae Choi <n10420398@qut.edu.au>
2019-04-29 13:30:23 +1000

    Finished general battle loop layout

---
+++ Pokemon/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Pokemon
+{
+    class Trainer
+    {
+        private string name;
+        private bool isAlive = true; // False if all pokemon have fainted
+        private int numberOfPkmn;
+        private double money;
+
+        public Trainer(string name, int numberOfPkmn, double money)
+        {
+            this.name = name;
+            this.numberOfPkmn = numberOfPkmn;
+            this.money = money;
+        }
+
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public bool IsAlive // returns trainer's current life status
+        {
+            get { return this.isAlive; }
+            set { this.isAlive = value; }
+        }
+
+        public void TrainerDies() //Call when trainer loses all pokemon
+        {
+            this.isAlive = false;
+        }
+        
+        public int NumberOfPkmn
+        {
+            get { return this.numberOfPkmn; }
+            set { this.numberOfPkmn = value; }
+        }
+        public double Money
+        {
+            get { return this.money; }
+            set { this.money = value; }
+        }
+
+        public void PkmnFainted() //decrements number of pokemon alive
+        {
+            this.numberOfPkmn -= 1;
+        }
+
+
+    }//End of class
+
+
+}//End of namespace
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Pokemon
 {
-    class Program
+    class Battle
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            bool battleFinished = false;
+
+            Trainer Red = new Trainer("red", 6, 200);
+
+            //Creates a list of Elite Four trainers
+            List<Trainer> eliteFour = new List<Trainer>()
+            {
+                new Trainer("Lorelei", 5, 2000),
+                new Trainer("Bruno", 5, 1000),
+                new Trainer("Agatha", 5, 3000),
+                new Trainer("Lance", 5, 5000)
+            };
+
+            while (!battleFinished) //Battle loop
+            {
+                for (int i = 0; i < 4; i++) // Loop through the eliteFour
+                {
+                    while (Red.NumberOfPkmn > 0 && 
+                            eliteFour[i].NumberOfPkmn > 0)//Battle code here
+                    {
+                        WriteLine("{0} has {1} pkmn left!", eliteFour[i].Name, eliteFour[i].NumberOfPkmn);
+
+                        eliteFour[i].PkmnFainted();
+
+                        ReadKey();
+                        
+                    }
+
+                    Red.PkmnFainted();
+                    WriteLine("{0}'s pkmn fainted!", Red.Name);
+
+                    WriteLine("{0} has {1} pkmn left!", Red.Name, Red.NumberOfPkmn);
+
+                }
+
+
+
+                WriteLine("Red defeated the Elite Four!");
+
+                battleFinished = true;
+
+            }//End of Battle loop
+
+            ReadKey();
+
+        }//End of Main program
+
+
+
+
+    }//End of Battle program
+}//End of namespace
